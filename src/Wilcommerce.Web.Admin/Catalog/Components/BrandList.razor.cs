d97dd4272193bb0a0401a8b83c21f97358fafd55
Alberto Mori <albi.mo87@hotmail.it>
2020-06-01 14:09:09 +0200

    WIP brand list

---
+++ src/Wilcommerce.Web.Admin/Catalog/Components/BrandList.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Wilcommerce.Catalog.Admin.Models.Brands;
+
+namespace Wilcommerce.Web.Admin.Catalog.Components
+{
+    public partial class BrandList
+    {
+        [Inject]
+        public NavigationManager Navigation { get; set; }
+
+        [Parameter]
+        public BrandListModel Brands { get; set; }
+
+        private BrandListQueryModel queryModel = new BrandListQueryModel();
+
+        void OpenBrandDetail(BrandListModel.ListItem item)
+        {
+            var url = $"catalog/brands/{item.Id}";
+            Navigation.NavigateTo(url);
+        }
+    }
+}
