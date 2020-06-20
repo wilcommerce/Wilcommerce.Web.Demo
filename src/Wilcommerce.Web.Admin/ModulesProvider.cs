6ad4756936691b69c5f779fab653f0fbe24f88e3
Alberto Mori <albi.mo87@hotmail.it>
2020-06-20 19:36:58 +0200

    add class to collect modules assemblies

---
+++ src/Wilcommerce.Web.Admin/ModulesProvider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace Wilcommerce.Web.Admin
+{
+    public static class ModulesProvider
+    {
+        public static IEnumerable<Assembly> AvailableModules => new[]
+        {
+            typeof(Wilcommerce.Catalog.Admin.UI.Blazor._Imports).Assembly
+        };
+    }
+}
