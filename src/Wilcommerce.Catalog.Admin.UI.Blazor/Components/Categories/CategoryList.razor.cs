f1316ae588553326ec8fe91a9bf28d7383bbcafc
Alberto Mori <albi.mo87@hotmail.it>
2020-07-25 15:25:11 +0200

    setup categories page

---
+++ src/Wilcommerce.Catalog.Admin.UI.Blazor/Components/Categories/CategoryList.razor.cs
@@ -0,0 +1,68 @@
+﻿using Blazorise;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+using Wilcommerce.Catalog.Admin.Models.Categories;
+
+namespace Wilcommerce.Catalog.Admin.UI.Blazor.Components.Categories
+{
+    public partial class CategoryList
+    {
+        [Parameter]
+        public CategoryListModel Categories { get; set; }
+
+        [Parameter]
+        public EventCallback<CategoryListModel.ListItem> OnCategoryDetailOpened { get; set; }
+
+        [Parameter]
+        public EventCallback<CategoryListModel.ListItem> OnCategoryDeleteConfirmed { get; set; }
+
+        [Parameter]
+        public EventCallback<CategoryListModel.ListItem> OnCategoryRestoreConfirmed { get; set; }
+
+        private Modal confirmDeleteModal;
+
+        private Modal confirmRestoreModal;
+
+        private CategoryListModel.ListItem selectedCategory;
+
+        async Task OpenCategoryDetail(CategoryListModel.ListItem item) => await OnCategoryDetailOpened.InvokeAsync(item);
+
+        void DeleteCategory(CategoryListModel.ListItem item)
+        {
+            selectedCategory = item;
+            confirmDeleteModal.Show();
+        }
+
+        void RestoreCategory(CategoryListModel.ListItem item)
+        {
+            selectedCategory = item;
+            confirmRestoreModal.Show();
+        }
+
+        async Task ConfirmDeleteCategory()
+        {
+            try
+            {
+                await OnCategoryDeleteConfirmed.InvokeAsync(selectedCategory);
+            }
+            finally
+            {
+                selectedCategory = null;
+            }
+        }
+
+        async Task ConfirmRestoreCategory()
+        {
+            try
+            {
+                await OnCategoryRestoreConfirmed.InvokeAsync(selectedCategory);
+            }
+            finally
+            {
+                selectedCategory = null;
+            }
+        }
+
+        void CloseModal(Modal modal) => modal.Hide();
+    }
+}
---
+++ src/Wilcommerce.Catalog.Admin.UI.Blazor/Pages/Categories/Categories.razor.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+using Wilcommerce.Catalog.Admin.Models.Categories;
+using Wilcommerce.Catalog.Admin.UI.Blazor.Services.Http;
+
+namespace Wilcommerce.Catalog.Admin.UI.Blazor.Pages.Categories
+{
+    public partial class Categories
+    {
+        [Inject]
+        public CategoriesHttpClient Client { get; set; }
+
+        [Inject]
+        public NavigationManager Navigation { get; set; }
+
+        private CategoryListModel categories;
+
+        private CategoryListQueryModel queryModel;
+
+        private bool loading;
+
+        public Categories()
+        {
+            queryModel = new CategoryListQueryModel();
+            loading = false;
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadCategories();
+        }
+
+        async Task LoadCategories()
+        {
+            loading = true;
+
+            try
+            {
+                categories = await Client.GetCategories(queryModel);
+            }
+            finally
+            {
+                loading = false;
+            }
+        }
+    }
+}
